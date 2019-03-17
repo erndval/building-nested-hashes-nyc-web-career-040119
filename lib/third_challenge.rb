8a688f243abe45089cc9e6302624a9fc8eb23fa1
Ernie Val <erndval@gmail.com>
2019-03-17 20:55:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         name: "Lady Montague", age: "54"
         },
       :hero => {
-        name: "Romeo", age: "15"
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
@@ -19,7 +19,7 @@ def third_challenge
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
-      :heroine => {name: "Juliet", age: "15"
+      :heroine => {name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
